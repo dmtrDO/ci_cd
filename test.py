74d6349023116706aad074910c8fbbae6c9ebd0c
dmtrDO <dmytro.domaratskiy@gmail.com>
2026-05-02 12:33:41 +0300

    task 5 + test1 (add html filter test)

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
 
-def test_simple_check():
-    # проста перевірка, щоб pytest мав що протестувати
-    assert True
+from bs4 import BeautifulSoup
+from crawler import Crawler
 
+def test_simple_word_filter():
+    html = "<html>" \
+                "<body>" \
+                    "<h1>Привіт!</h1>" \
+                    "<p>Це КРАУЛЕР і він працює.</p>" \
+                "</body>" \
+            "</html>"
+    
+    soup = BeautifulSoup(html, "lxml")
+    
+    result = Crawler._Crawler__filter_page(soup)
+
+    assert "привіт" in result
+    assert "краулер" in result
+    assert "і" not in result
+    assert "це" not in result
+    assert "працює" in result
 
